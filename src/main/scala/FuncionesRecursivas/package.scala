fb7bdf1ba79ec89805faf10e3bdb1f82286287a7
Valentina-Nitola <valentina.nitola@correounivalle.edu.co>
2025-08-30 00:42:16 -0500

    first commit

---
+++ src/main/scala/FuncionesRecursivas/package.scala
@@ -0,0 +1,15 @@
+package object FuncionesRecursivas {
+  def maxLin(1:List[Int]): Int = {
+
+  }
+  def maxIt(1:List[Int]): Int = {
+
+  }
+  def movsTorresHanoi(n:Int): BigInt = {
+
+  }
+  def torresHanoi(n:Int, t1:Int, t2:Int, t3:Int): BigInt = {
+    
+  }
+  
+}
